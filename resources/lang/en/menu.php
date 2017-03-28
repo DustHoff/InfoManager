c803640db29b1e32dc06d880cfbbb762a7c1b22f
dusthoff <dustinh@onesi.de>
2017-03-28 19:35:06 +0200

    #6 menu info en local

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    "save" => "Save",
+    "delete" => "Delete",
+    "create" => "Create",
+
+    "systems" => "Systems",
+    "dates" => "Dates",
+    "batch" => "Batch",
+    "schedule" => "Schedule",
+    "search" => "search",
+
+    "user" => "User",
+    "group" => "Group",
+];
