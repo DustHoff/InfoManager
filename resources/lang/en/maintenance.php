d5a65fcc992f545b92ec482774748325f9bf0cb9
dusthoff <dustinh@onesi.de>
2017-03-28 19:03:41 +0200

    #6 maintenance en local

---
+++ resources/lang/en/maintenance.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    /**
+     * Maintenance Types
+     */
+    "maintenance" => "Maintenance",
+    "incident" => "Incident",
+    "restricted" => "Restricted Function/Performance",
+
+    /**
+     * Maintenance States
+     */
+    "announced" => "Announced",
+    "active" => "Active",
+
+    /**
+     * Maintenance messages
+     */
+    "from" => "From :start",
+    "till" => "till :end"
+];
