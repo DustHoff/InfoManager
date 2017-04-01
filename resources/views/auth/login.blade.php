ff05aeaa83d1876ae4f74965b3510cb9c450b5dc
dusthoff <dustinh@onesi.de>
2017-04-01 08:00:58 +0200

    redesigned login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,30 @@
 @extends("layout.master")
 @section("content")
-    <form method="post" action="{{route("login")}}">
-        <div class="panel panel-danger" style="width:500px;margin: 0 auto">
-            <div class="panel-heading">
-                <h3 class="panel-title">Login</h3>
-            </div>
-            <div class="panel-body">
-                <label for="username">@lang("menu.username")</label><input id="username" type="text" name="name"><br>
-                <label for="password">@lang("menu.password")</label><input id="password" type="password"
-                                                                           name="password"><br>
-                {{csrf_field()}}
-                <button type="submit" class="btn btn-success">Login</button>
-                @include("layout.error")
-            </div>
+    <div class="panel panel-danger" style="width:500px;margin: 20px auto;">
+        <div class="panel-heading">
+            <h3 class="panel-title">Login</h3>
         </div>
-    </form>
+        <div class="panel-body">
+            <form method="post" action="{{route("login")}}" class="form-horizontal">
+                <div class="form-group">
+                    <div class="col-sm-4 control-label">@lang("menu.username")</div>
+                    <div class="col-sm-8">
+                        <input id="username" type="text" name="name" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 control-label">@lang("menu.password")</div>
+                    <div class="col-sm-8">
+                        <input id="password" type="password" name="password" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-9">{{csrf_field()}}</div>
+                    <div class="col-sm-3">
+                        <button type="submit" class="btn btn-success" style="width: 100%">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
