0bd7859b768f7a60fc752953b84f2d7b9f54d09c
dusthoff <dustinh@onesi.de>
2017-04-01 18:20:00 +0200

    #6 last translations

--- resources/lang/de/auth.php
+++ resources/lang/de/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Benutzername und Passwort sind falsch.',
+    'throttle' => 'Zu viele Loginversuche! Bitte versuchen Sie es in :seconds Sekunden erneut.',
 
 ];
