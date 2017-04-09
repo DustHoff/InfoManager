6c2ed85ea6a170133a8c1afbad1c43055e9b9d37
dusthoff <dustinh@onesi.de>
2017-04-09 13:59:22 +0200

    #3 fixed some permission bugs

--- resources/views/info/Application/master.blade.php
+++ resources/views/info/Application/master.blade.php
@@ -4,47 +4,51 @@
         <li role="presentation" class="active"><a href="#info" aria-controls="information" role="tab"
                                                   data-toggle="tab">@lang("menu.info")</a>
         </li>
-        <li role="presentation"><a href="#schedule" aria-controls="schedule" role="tab"
-                                   data-toggle="tab">@lang("menu.schedule")</a>
-        </li>
+        @can("schedule",$maintainable)
+            <li role="presentation"><a href="#schedule" aria-controls="schedule" role="tab"
+                                       data-toggle="tab">@lang("menu.schedule")</a>
+            </li>
+        @endcan
         <li role="presentation"><a href="#components" aria-controls="components" role="tab"
                                    data-toggle="tab">@lang("menu.dependency")</a></li>
     </ul>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="info">
             @component("info.Maintainable.info")
-            @slot("url") {{route("updateApplication",["application" => $maintainable->maintainable])}} @endslot
-            @slot("name") {{$maintainable->name}} @endslot
-            @slot("desc") {{$maintainable->desc}} @endslot
-            @slot("type") {{$maintainable->maintainable_type}} @endslot
-            @slot("host"){{$maintainable->maintainable->host_id}}@endslot
-            @slot("contacts")
-            @foreach($maintainable->emails as $email)
-                <option value="{{$email->email}}">{{$email->email}}</option>
-            @endforeach
-            @endslot
+                @slot("url") {{route("updateApplication",["application" => $maintainable->maintainable])}} @endslot
+                @slot("name") {{$maintainable->name}} @endslot
+                @slot("desc") {{$maintainable->desc}} @endslot
+                @slot("type") {{$maintainable->maintainable_type}} @endslot
+                @slot("host"){{$maintainable->maintainable->host_id}}@endslot
+                @slot("contacts")
+                    @foreach($maintainable->emails as $email)
+                        <option value="{{$email->email}}">{{$email->email}}</option>
+                    @endforeach
+                @endslot
             @endcomponent
         </div>
-        <div role="tabpanel" class="tab-pane" id="schedule">
-            @component("info.Maintenance.schedule")
-            @slot("infected")
-            <div class="list-group">
-                <div class="list-group-item">@lang("menu.infectedsystems")</div>
-                @foreach($maintainable->infect() as $infectable)
-                    @component("info.Maintainable.item")
-                    @slot("url") {{route("maintainable",["maintainable"=>$infectable])}} @endslot
-                    {{Maintainable::find($infectable)->name}}
-                    @endcomponent
-                @endforeach
+        @can("schedule",$maintainable)
+            <div role="tabpanel" class="tab-pane" id="schedule">
+                @component("info.Maintenance.schedule")
+                    @slot("infected")
+                        <div class="list-group">
+                            <div class="list-group-item">@lang("menu.infectedsystems")</div>
+                            @foreach($maintainable->infect() as $infectable)
+                                @component("info.Maintainable.item")
+                                    @slot("url") {{route("maintainable",["maintainable"=>$infectable])}} @endslot
+                                    {{Maintainable::find($infectable)->name}}
+                                @endcomponent
+                            @endforeach
+                        </div>
+                        <input type="hidden" id="maintainable_id" name="maintainable[]"
+                               value="{{$maintainable->id}}">
+                    @endslot
+                    @foreach($maintainable->maintenances as $maintenance)
+                        @include("info.Maintenance.item")
+                    @endforeach
+                @endcomponent
             </div>
-            <input type="hidden" id="maintainable_id" name="maintainable[]"
-                   value="{{$maintainable->id}}">
-            @endslot
-            @foreach($maintainable->maintenances as $maintenance)
-                @include("info.Maintenance.item")
-            @endforeach
-            @endcomponent
-        </div>
+        @endcan
         <div role="tabpanel" class="tab-pane" id="components">@include("info.Application.components")</div>
     </div>
 @endsection
