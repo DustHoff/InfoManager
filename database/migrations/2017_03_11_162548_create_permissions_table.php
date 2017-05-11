45dceda34b656601094c5939b5594c30cbe53e24
Dustin Hoffmann <dustinh@onesi.de>
2017-05-11 07:30:41 +0200

    keep migrations

---
+++ database/migrations/2017_03_11_162548_create_permissions_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char("name");
+            $table->char("permission");
+        });
+        DB::table('permissions')->insert([
+            [
+                "name" => "Schedule Maintenance",
+                "permission" => \App\Http\Requests\MaintenanceRequest::class
+            ], [
+                "name" => "create/edit Systems",
+                "permission" => \App\Http\Requests\MaintainableRequest::class
+            ], [
+                "name" => "create/edit Groups",
+                "permission" => \App\Http\Requests\GroupRequest::class
+            ], [
+                "name" => "create/edit Users",
+                "permission" => \App\Http\Requests\UserRequest::class
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
