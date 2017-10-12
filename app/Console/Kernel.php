209c2cb41181e47d0e757bbf1da55625c7ac154e
dusthoff <dustinh@onesi.de>
2017-10-12 08:47:12 +0200

    added test command for ESXI communication

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\checkMailConfig;
+use App\Console\Commands\pullESXI;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        checkMailConfig::class
+        checkMailConfig::class,
+        pullESXI::class
     ];
 
     /**
