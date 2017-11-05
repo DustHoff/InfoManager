a107f2945ad44e0299ccf3b4fa04b55e8d3d75ea
dusthoff <dustinh@onesi.de>
2017-11-05 13:07:21 +0100

    fullcalendar as vue component

---
+++ resources/assets/js/components/calendar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    props: {
+        events: {
+            required: true
+        },
+    },
+
+    data: function () {
+        return {
+            cal: null
+        }
+    },
+
+    mounted: function () {
+        var self = this;
+        self.cal = $(self.$el);
+
+
+        var args = {
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            height: "auto",
+            allDaySlot: false,
+            slotEventOverlap: false,
+            timeFormat: 'HH:mm',
+
+            events: self.events,
+            startParam: "maintenance_start",
+            endParam: "maintenance_end"
+        };
+        this.cal.fullCalendar(args);
+
+    }
+}
+</script>
