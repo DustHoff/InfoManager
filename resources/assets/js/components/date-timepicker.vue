07050a48508c67005279fab5863e35166d8e463c
dusthoff <dustinh@onesi.de>
2017-11-05 13:07:42 +0100

    date-timepicker as vue component

---
+++ resources/assets/js/components/date-timepicker.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" class="form-control" v-bind:id="name" v-bind:name="name" v-bind:placehoder="placeholder"/>
+</template>
+
+<script>
+export default {
+    props: ["name", "placeholder", "min_picker"],
+    mounted: function () {
+        self = this;
+        $(this.$el).datetimepicker({
+            format: "YYYY-MM-DD HH:mm:ss",
+            useCurrent: (this.min_picker == null) ? true : false
+        });
+        if (this.min_picker != null) {
+            $("#" + this.min_picker).on("dp.change", function (e) {
+                $(self.$el).data("DateTimePicker").minDate(e.date);
+            });
+        }
+
+    }
+}
+</script>
