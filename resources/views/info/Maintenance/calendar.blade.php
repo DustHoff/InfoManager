da7eb0aa9c8c22ba8a8e52c7ed9ff549b9cfbe21
dusthoff <dustinh@onesi.de>
2017-11-05 13:11:54 +0100

    use vue js component

--- resources/views/info/Maintenance/calendar.blade.php
+++ resources/views/info/Maintenance/calendar.blade.php
@@ -1,21 +1,4 @@
 @extends("layout.master")
 @section("content")
-    <div id="calendar"></div>
-    <script>
-        $(document).ready(function () {
-            $("#calendar").fullCalendar({
-
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,listDay'
-                },
-                events: '{{route("calendarFeed")}}',
-                startParam: 'maintenance_start',
-                endParam: 'maintenance_end',
-                selectable: true,
-                locale: '{{env("local")}}'
-            });
-        });
-    </script>
+    <calendar events="{{route('calendarFeed')}}"/>
 @endsection
